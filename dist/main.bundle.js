6e03e2c7a53361bf5cdab01323c726d799571625
chroda <chroda@chroda.com.br>
2018-04-15 11:06:52 -0300

    pubish footer to test

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -20,7 +20,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /***/ "./src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "May the fourth be with you\n"
+module.exports = "<app-site></app-site>\n"
 
 /***/ }),
 
@@ -70,6 +70,7 @@ var AppComponent = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("./src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__site_site_component__ = __webpack_require__("./src/app/site/site.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -79,13 +80,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
             declarations: [
-                __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+                __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_3__site_site_component__["a" /* SiteComponent */]
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
@@ -95,6 +98,56 @@ var AppModule = /** @class */ (function () {
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/site/site.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<header>\n  HEADER\n</header>\n<div id=\"content\">\n  content yeah\n</div>\n\n\n<footer>\n  FOOTER\n  <div class=\"path\">\n    <p>\n      &#60; coded /&#62; with [] by chroda\n    </p>\n  </div>\n  <img src=\"../../assets/images/bb-8.png\" alt=\"bb-8\" class=\"bb-8\" />\n  <img src=\"../../assets/images/r2d2.png\" alt=\"r2d2\" class=\"r2d2\" />\n</footer>\n"
+
+/***/ }),
+
+/***/ "./src/app/site/site.component.scss":
+/***/ (function(module, exports) {
+
+module.exports = "header {\n  height: 200px;\n  background-color: chocolate; }\n\n#content {\n  width: 800px;\n  margin: 0 auto;\n  background-color: cadetblue; }\n\nfooter {\n  position: absolute;\n  bottom: 0;\n  height: 400px;\n  width: 100%; }\n\nfooter div.path {\n    height: 100%;\n    width: 100%;\n    background: -webkit-gradient(linear, left top, left bottom, from(#868686), to(white));\n    background: linear-gradient(to bottom, #868686 0%, white 100%);\n    position: absolute;\n    bottom: 0;\n    background-color: white;\n    -webkit-clip-path: polygon(48% 0%, 52% 0, 100% 100%, 0% 100%);\n            clip-path: polygon(48% 0%, 52% 0, 100% 100%, 0% 100%);\n    z-index: 0; }\n\nfooter div.path p {\n      color: darkgray;\n      position: absolute;\n      right: 35%;\n      bottom: 70px; }\n\nfooter img {\n    height: 400px;\n    position: absolute;\n    bottom: 0; }\n\nfooter img.bb-8 {\n    left: 160px; }\n\nfooter img.r2d2 {\n    right: 160px; }\n"
+
+/***/ }),
+
+/***/ "./src/app/site/site.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return SiteComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var SiteComponent = /** @class */ (function () {
+    function SiteComponent() {
+    }
+    SiteComponent.prototype.ngOnInit = function () {
+    };
+    SiteComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-site',
+            template: __webpack_require__("./src/app/site/site.component.html"),
+            styles: [__webpack_require__("./src/app/site/site.component.scss")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], SiteComponent);
+    return SiteComponent;
 }());
 
 
--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -3,7 +3,7 @@ webpackJsonp(["styles"],{
 /***/ "./node_modules/raw-loader/index.js!./node_modules/postcss-loader/lib/index.js??embedded!./node_modules/sass-loader/lib/loader.js??ref--8-3!./src/styles.scss":
 /***/ (function(module, exports) {
 
-module.exports = "body {\n  background-color: black;\n  color: whitesmoke;\n  text-align: center; }\n"
+module.exports = "@import url(https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css);\n/* RGB */\n@font-face {\n  font-family: 'ETHNOCENTRIC-RG';\n  font-style: normal;\n  font-weight: 400;\n  src: local(\"/src/assets/fonts/ETHNOCENTRIC-RG.TTF\") format(\"ttf\"); }\nbody {\n  background-color: black;\n  color: #ffe81f;\n  text-align: center; }\n"
 
 /***/ }),
 
