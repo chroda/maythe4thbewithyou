f2b4c427514ce5c30e3ca78c0211fc7b9e56876e
chroda <chroda@chroda.com.br>
2018-04-15 09:50:38 -0300

    layout starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SiteComponent } from './site/site.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteComponent
   ],
   imports: [
     BrowserModule
