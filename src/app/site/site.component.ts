1384e66a3ee642a89585b97bc36630ea01a14554
Christian Oliveira <coliveira@quanticosolutions.com>
2023-03-24 14:47:35 +0000

    update api

--- src/app/site/site.component.ts
+++ src/app/site/site.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-site',
-  templateUrl: './site.component.html',
-  styleUrls: ['./site.component.scss']
+  selector: "app-site",
+  templateUrl: "./site.component.html",
+  styleUrls: ["./site.component.scss"],
 })
-
 export class SiteComponent implements OnInit {
-
   dataset: any = [];
 
   constructor(private HttpClient: HttpClient) {}
 
   onKeyUp(value) {
-    this.HttpClient.get(`https://swapi.co/api/people/?search=${value}`).subscribe(
-      (data: any) => {
-        if (data) {
-          this.dataset = data.results;
-        }
+    this.HttpClient.get(
+      `https://swapi.dev/api/people/?search=${value}`
+    ).subscribe((data: any) => {
+      if (data) {
+        this.dataset = data.results;
       }
-    );
+    });
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
